1ecb08805f59bd2203f7b740f6300a39eda6b2ff
Andrey <andreykotcilo@gmail.com>
2020-01-05 15:55:23 +0300

    Project 1

---
+++ project/script.js
@@ -0,0 +1,20 @@
+// 'use strict'
+
+// let percone = {
+//     name: "Andrey",
+//     age: 24,
+// }
+
+// //let answer = confirm("Are you here ?")
+// //console.log(answer);
+
+// let x = prompt('Что Вы хотите узнать ?','')
+
+// console.log(percone.x);
+
+let x = 10,
+    y = 10;
+// x++;
+// y--;
+console.log(x+1);
+console.log(--y);
